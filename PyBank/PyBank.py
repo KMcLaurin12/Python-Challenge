fb4b7881d53a68baeb0263ddf3759b6f558cd151
Kiara <mclaurinkiara@gmail.com>
2021-04-05 20:10:54 -0400

    added my project

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,6 +1,26 @@
 import csv
 import os
-csv.path = "Resources/budget_data.csv"
+with open("Resources/budget_data.csv") as csv_file
+Months = 0
+Profits = []
+Dates = []
+csv_reader = csv.reader(csv_budget_data.csv)
 
-budget_df = pd.read_csv(csv.path)
-budget_df()
+for row in open("budget_data.csv"):
+num_rows += 1
+print(num_rows)
+
+csvreader = csv.reader(csv_file)
+first_row = next(csvreader)
+
+for row in csvreader:
+    total_months += 1
+    total_net_change += int(row[1])
+
+    print(net_change_list)
+
+  max_value = max(numbers)
+print('Maximum value:', max_value)
+
+mean_value = mean(numbers)
+print('Mean value:', mean_value)
